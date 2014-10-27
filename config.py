6fc185546c9e590411fa194adf680961f9096828
Amarpreet Minhas <amarpreet@minhas.io>
2014-10-27 14:39:12 -0400

    Removed config

--- config.py
+++
@@ -1,9 +0,0 @@
-bots = [
-    {
-        'nick' : 'testbangbot',
-        'network' : 'chat.freenode.net',
-        'channel' : '#devvul',
-        'password' : '',
-        'ssl' : True,
-    },
-]
